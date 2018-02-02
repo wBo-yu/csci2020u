89ea2076b91b9ae883ea90bcf03d36e5eda72548
BoYu <wen.yu@uoit.net>
2018-02-02 16:03:10 -0500

    add CSVDemo

---
+++ lab02/src/CSVDemo.java
@@ -0,0 +1,18 @@
+import org.apache.commons.csv.*;
+public class CSVDemo {
+public static void main(String[] args) {
+  try {
+      Reader in = new FileReader("data.csv");
+      Iterable<CSVRecord> records = CSVFormat.DEFAULT.withHeader("First Name", "Last Name", "SID","Grade").parse(in);
+
+      for (CSVRecord record: records) {
+          String lastName = record.get("Last Name");
+          String firstName = record.get("First Name");
+          String grade = record.get("Grade");
+          System.out.println(firstName + " " + lastName + " (" + grade + ")");
+        }
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+}
